7afadbc05b0e3205644fc6526639b3e57732af66
Celestially93 <celestialdestacamento1018@gmail.com>
2020-10-15 20:11:04 +0800

    pounds to kilograms

--- cit160/script/poundstokilograms.js
+++ cit160/script/poundstokilograms.js
@@ -1,10 +1,10 @@
-function convertpoundstokilograms () {
+function poundstoKilograms() {
     //Input: Get the number in pounds from the user.
-    let pounds = parseFloat (document.getElementById ("poundsInput").value);
+    let pounds = parseFloat(document.getElementById("poundsInput").value);
 
     //Processing: Convert 
     let kilograms = pounds / 2.2046;
 
-//Output
-document.getElementById ("output").innerHTML = kilograms.toFixed(1) + " " + "Kilograms";
+    //Output
+    document.getElementById("output").innerHTML = kilograms.toFixed(1) + " " + "Kilograms";
 }
