3987e993e970506f3da29c5c70da3816e3e333fa
Celestially93 <celestialdestacamento1018@gmail.com>
2020-10-23 19:11:22 +0800

    footwear

---
+++ cit160/script/footwear.js
@@ -0,0 +1,19 @@
+function footWear() {
+    //Input: Get the weather from the user.
+    let weather = document.getElementById("weather-select").value;
+    //Process: 
+    let shoes;
+    if (weather == "hot") {
+        shoes = "Sandals,";
+    } else if (weather == "rain") {
+        shoes = "Galoshes,";
+    } else if (weather == "snow") {
+        shoes = "Boots,";
+    } else {
+        shoes = "Your favorite shoes,";
+    }
+
+    //Output: Display the shoes for "today's weather" to the user.
+    document.getElementById("output").innerHTML = `${shoes} is your footwear for today's weather!`;
+
+}
