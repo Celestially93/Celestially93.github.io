df5cb2c471c88adc1373edf8c5743927de2549fe
Celestially93 <celestialdestacamento1018@gmail.com>
2020-11-23 22:41:07 +0800

    fivetimes

--- cit160/script/fiveTimes.js
+++ cit160/script/fiveTimes.js
@@ -1,4 +1,5 @@
 let output = "";
-for (let i = 1; 1 <= 10; i++){
-    output += "5 x i" + i * 5 + '<br>';
+for (let i = 1; i <= 12; i++) {
+    output += "5 x " + i + " = " + i * 5 + '<br>';
 }
+document.getElementById("output").innerHTML = output;
