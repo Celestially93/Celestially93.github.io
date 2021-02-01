76267b3b55c3d59568f67d4dc943c65daeeec443
Celestially93 <celestialdestacamento1018@gmail.com>
2021-02-01 14:57:57 +0800

    commit

--- cit160/script/sumOdds.js
+++ cit160/script/sumOdds.js
@@ -2,7 +2,7 @@ function sumofOddnum() {
     //Input: The user inputs the number.
     //Process: Adds all the the odd numbers between 1 and the number  that the user entered using "for loop".
     //Outputs the result.
-    let n = parseFloat(document.getElementById("number").value);
+    let n = parseInt(document.getElementById("number").value);
     let sum = 0;
     for (let i = 1; i <= n; i++) {
         if (i % 2 !== 0) {
