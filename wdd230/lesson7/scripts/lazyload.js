ca41edc2e902a4ef7672a2823300ebfc3ecbd2bb
Celestially93 <celestialdestacamento1018@gmail.com>
2021-02-26 20:49:22 +0800

    commit

--- wdd230/lesson7/scripts/lazyload.js
+++ wdd230/lesson7/scripts/lazyload.js
@@ -1,4 +1,4 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+/* let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
@@ -18,6 +18,41 @@ imagesToLoad.forEach((img) => {
         }
       });
     });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  } */
+
+  let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 300px 0px"
+};
+
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+  if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    },imgOptions);
     imagesToLoad.forEach((img) => {
       observer.observe(img);
     });
