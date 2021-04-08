f7acdd8558b3df2f285f4d11396e1c6371f6fdaf
Celestially93 <celestialdestacamento1018@gmail.com>
2021-04-09 00:17:35 +0800

    commit

--- wdd230/lesson14/scripts/weather.js
+++ wdd230/lesson14/scripts/weather.js
@@ -20,32 +20,6 @@ fetch(apiURL)
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
-//Three Day Forecast
-
-/*const apiForecastURL = "api.openweathermap.org/data/2.5/forecast/daily?lat=13.6167&lon=123.1833&cnt=3&appid=632c14c053cc10b594e70f5cb1b4d0f1";
-
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-
-    const dayOfWeek = ""
-    let day = new Date;
-    
-    for (let day = 0; day <= 3; day ++) {
-      let day = dayOfWeek;
-
-    document.getElementById("days1").textContent = day;
-    document.getElementById("days2").textContent = day;
-    document.getElementById("days3").textContent = day;
-
-    document.getElementById("day1").textContent = jsObject.current.temp;
-    document.getElementById("day2").textContent = jsObject.current.temp;
-    document.getElementById("day3").textContent = jsObject.current.temp;
-    
-    }
-
-});
-*/
 
 const apiForecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=1698829&APPID=632c14c053cc10b594e70f5cb1b4d0f1&units=metric";
 
